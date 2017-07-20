210695cca0540f5b1b617daade049b18ad15db34
Antoha5001 <plantsvzzombi@mail.ru>
2017-07-20 18:03:50 +0700

    'lab3'

--- office/create_tables for modules 3.sql
+++ office/create_tables for modules 3.sql
@@ -4,14 +4,18 @@ DESC lessons;
 CREATE DATABASE IF NOT EXISTS course;
 USE course;
 SHOW TABLES ;
+
+-- Создание БД
+DROP TABLE IF EXISTS teachers;
 CREATE TABLE IF NOT EXISTS teachers
 (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Код учителя',
   name VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Имя учителя',
   addr VARCHAR(200) NOT NULL DEFAULT '' COMMENT 'Адрес учителя',
-  PRIMARY KEY (id)
+  CONSTRAINT pkId PRIMARY KEY (id)
 ) COMMENT 'Таблица учителя с первичным ключом';
 
+DROP TABLE IF EXISTS courses;
 CREATE TABLE IF NOT EXISTS courses
 (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Код курса',
@@ -19,16 +23,24 @@ CREATE TABLE IF NOT EXISTS courses
   length TINYINT NOT NULL DEFAULT 0 COMMENT 'Продолжительность курса, часов',
   description VARCHAR(200) NOT NULL DEFAULT '' COMMENT 'Описание курса',
   cathegory VARCHAR(50) NOT NULL DEFAULT '' COMMENT 'Категория курса',
-  PRIMARY KEY (id)
+  previous INT DEFAULT 0 COMMENT 'Требования к курсу',
+  CONSTRAINT pkId PRIMARY KEY (id),
+  INDEX ixPrevious (previous),
+  CONSTRAINT fkPrevious FOREIGN KEY (previous) REFERENCES (id)
 ) COMMENT 'Таблица курсов с первичным ключом';
 
+DROP TABLE IF EXISTS lessons;
 CREATE TABLE IF NOT EXISTS lessons
 (
-  id INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Код курса',
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Код урока',
   teacher INT,
   course INT,
   room CHAR(5) NOT NULL DEFAULT 0 COMMENT 'Номер кабинета',
   length TINYINT UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Продолжительность урока, часов',
   lesson_date DATETIME COMMENT 'Дата урока',
-  PRIMARY KEY (id)
+  CONSTRAINT pkId PRIMARY KEY (id),
+  INDEX ixTeacher (teacher),
+  CONSTRAINT fkTeacher FOREIGN KEY (teacher) REFERENCES teachers(id),
+  INDEX ixCourse (course),
+  CONSTRAINT fkCourse FOREIGN KEY (course) REFERENCES courses(id)
 ) COMMENT 'Таблица уроков с первичным ключом';
--- office/information-schema.sql
+++ office/information-schema.sql
@@ -12,6 +12,11 @@ SELECT TABLE_NAME,TABLE_COMMENT, TABLE_SCHEMA
 FROM information_schema.TABLES
 WHERE TABLE_SCHEMA = 'course';
 
+-- Спецификация колонок для таблицы table 2 БД module3
 SELECT COLUMN_NAME, data_type, COLUMN_COMMENT
-        FROM information_schema.COLUMNS
-        WHERE COLUMN_NAME =;
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = 'module3'
+      AND TABLE_NAME = 'table2';
+
+-- Список БД
+SELECT * FROM information_schema.SCHEMATA \G;
